f1be234d4b408f8a786b18decbdfcb47ebc0fac7
LilaVandenBosch <56552137+LilaVandenBosch@users.noreply.github.com>
2019-11-19 12:54:28 -0500

    Add files via upload

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,29 @@
+window.alert("Hello, World!");
+
+document.write('Hello, World!');
+
+var A= "This is a string";
+
+document.write(A);
+
+var b= "String 2";
+
+window.alert("b");
+
+document.write("\"Be who you are and say what you feel,"
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++"-Dr. Seuss");
+
+var B= "Concatenated" + "String"
+var B= B.fontcolor("red");
+document.write(B)
+
+var Family= "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+vat family= family.fontcolor("green");
+document.write(Dad);
+
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3+3)
