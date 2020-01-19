14705d624d00e88d5ca19f2e507a2ba1bc53bd25
LilaVandenBosch <56552137+LilaVandenBosch@users.noreply.github.com>
2020-01-19 11:51:01 -0500

    javascript 5

---
+++ Basic_JavaScript_5.js
@@ -0,0 +1,61 @@
+// class Vehicle {}
+
+// public class Car extends Vehicle {
+//     public static void main(String args[]) {
+//         Vehicle a = new Car();
+//         boolean result = a instanceof Car;
+//         MediaKeySystemAccess.out.printIn( result );
+//     }
+// }
+
+function my_Function() {
+    document.getElementById("test").innerHTML = 0/0;
+}
+
+function my_Function2() {
+    document.getElementById("test2").innerHTML = isNaN('This is a string');
+}
+
+function my_Function3() {
+    document.getElementById("test3").innerHTML = isNaN('007');
+}
+
+// document.write(2E310);
+
+// document.write(-3E310);
+
+// document.write(10 > 2);
+
+// document.write(10 < 2);
+
+console.log(7+7);
+
+// document.write("10" + 5);
+
+// document.write(10 == 10);
+
+// document.write(3 == 11);
+
+// X = 10;
+// Y = 10;
+// document.write(X === Y);
+
+// X = 82;
+// Y = "82";
+// document.write(X === Y);
+
+// A = "Magnus";
+// B = "Magnus";
+// document.write(A === B)
+
+// document.write(5 > 2 && 10 > 4);
+
+// document.write(5 > 10 && 10 > 4);
+
+// document.write(5 > 10 || 10 > 4);
+
+// document.write(5 > 10 || 10 > 20);
+
+
+
+
